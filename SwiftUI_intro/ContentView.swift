e6285af890790f7344d8bdba8576a01bd963c225
Abdul <abdul.hassan1@alaskaair.com>
2024-03-28 18:38:37 -0700

    Initial Commit

---
+++ SwiftUI_intro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_intro
+//
+//  Created by Abdul Hassan on 3/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_intro/SwiftUI_introApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_introApp.swift
+//  SwiftUI_intro
+//
+//  Created by Abdul Hassan on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_introApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
